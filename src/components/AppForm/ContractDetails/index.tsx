2d12ca6fa2a850f188addaac601288513c56fd4c
vaibhavchellani <vaibhavchellani223@gmail.com>
2019-01-26 17:41:44 +0530

    added stepper

--- src/components/AppForm/ContractDetails/index.tsx
+++ src/components/AppForm/ContractDetails/index.tsx
@@ -5,7 +5,29 @@ import * as Yup from 'yup'
 import { Formik, FieldArray } from 'formik'
 import { Form, Text, FormButton, Tags } from 'elements'
 import { Icon } from 'sharedComponent'
+import { makeStyles } from '@material-ui/styles'
+import Stepper from '@material-ui/core/Stepper'
+import Step from '@material-ui/core/Step'
+import StepButton from '@material-ui/core/StepButton'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 
+function getSteps() {
+  return ['Select campaign settings', 'Create an ad group', 'Create an ad']
+}
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return 'Step 1: Select campaign settings...'
+    case 1:
+      return 'Step 2: What is an ad group anyways?'
+    case 2:
+      return 'Step 3: This is the bit I really care about!'
+    default:
+      return 'Unknown step'
+  }
+}
 type formValue = {
   name: string
   address: string
@@ -29,6 +51,8 @@ export default class ContractDetails extends Component {
   }
 
   public render() {
+    const steps = getSteps()
+
     return (
       <Formik
         initialValues={{ ...this.init }}
@@ -58,6 +82,13 @@ export default class ContractDetails extends Component {
         }) => {
           return (
             <Form.Box onSubmit={handleSubmit}>
+              <Stepper nonLinear activeStep={0}>
+                {steps.map((label, index) => (
+                  <Step key={label}>
+                    <StepButton>{label}</StepButton>
+                  </Step>
+                ))}
+              </Stepper>
               <Form.Content>
                 <Text size={1.75} themeColor bold>
                   Let’s narrow down your options
